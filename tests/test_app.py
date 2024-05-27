38e5a1fc06021792c2d5d1d68e67bb19101c2163
Massimo Biagioli <biagiolimassimo@gmail.com>
2024-05-27 12:14:56 +0200

    fix test

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,4 +2,4 @@ def test_root(client):
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+    assert response.json() == {"health": "ok"}
